e1811e1809770cb6eae382518931312d2c23305d
Guillaume Wuip <guillaume@wuips.com>
2015-09-02 22:11:02 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+(function () {
+
+    'use strict';
+
+    var http      = require('http'),
+        query     = require('querystring'),
+        request   = require('request');
+
+    var PORT = 8080;
+
+    http.createServer(function (req, res) {
+
+        if (req.url.indexOf('?') >= 0) {
+            var params = query.parse(req.url.replace(/^.*\?/, ''));
+
+            var user = params.user,
+                pwd  = params.password,
+                url  = params.url;
+        }
+
+        if (user && pwd && url) {
+
+            var target = 'https://' + user + ':' + pwd + '@' + url;
+            console.log(target);
+
+            request(target).pipe(res);
+        } else {
+            res.writeHead(400);
+            res.end('error with params');
+        }
+
+    }).listen(PORT, function () {
+        console.log('Listenning on port ', PORT);
+    });
+
+}());
